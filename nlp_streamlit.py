87cf0680d65195083e10674fec7bc3ab47541d20
z0z0z0z0z0 <131263912+z0z0z0z0z0@users.noreply.github.com>
2024-05-10 23:58:42 +0800

    Update nlp_streamlit.py

--- nlp_streamlit.py
+++ nlp_streamlit.py
@@ -6,9 +6,9 @@ from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
 import time
 
 # Load RoBERTa model and tokenizer
-model_name = "deepset/roberta-base-squad2"
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+#model_name = "deepset/roberta-base-squad2"
+#model = AutoModelForQuestionAnswering.from_pretrained(model_name)
+#tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 st.title("PDF GPT")
 
